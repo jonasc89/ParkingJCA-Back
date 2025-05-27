5ae5bab1434014732bbd399130d9d71cd8c3b67d
samuelarredondo <arredondosamuel0@gmail.com>
2025-05-27 17:40:02 -0500

    se agregan endpoints al back para registrar usuarios y uno para mostrar cuales son las placas que se encuentran en el parqueadero

--- src/routes/registerpersonal.routes.js
+++ src/routes/registerpersonal.routes.js
@@ -1,11 +1,16 @@
 import isAuth from '../middlewares/auth.middleware.js';
 import { Router } from 'express';
 import {
+  deletePersonal,
+  updatePersonal,
   createEntry, list
 } from '../controllers/registerpersonal.controller.js';
 
 const r=Router();
 r.post('/',isAuth, createEntry) ;
 r.get('/list', isAuth, list);
+r.put('/update/:id',isAuth, updatePersonal);
+r.delete('/delete/:id',isAuth, deletePersonal);
+
 
 export default r;
